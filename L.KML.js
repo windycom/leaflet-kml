0c4f2bf76f4a377bf585e5d9799791b64f51fa24
Sam <sam@1485048.no-reply.drupal.org>
2019-11-20 10:28:02 +0800

    Do not wait for features to be added to the map to bind the popup.

--- L.KML.js
+++ L.KML.js
@@ -253,9 +253,7 @@ L.Util.extend(L.KML, {
     }
 
     if (name) {
-      layer.on('add', function () {
-        layer.bindPopup('<h2>' + name + '</h2>' + descr, { className: 'kml-popup'});
-      });
+      layer.bindPopup('<h2>' + name + '</h2>' + descr, { className: 'kml-popup'});
     }
   },
 
